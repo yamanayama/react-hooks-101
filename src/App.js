aba80833848ef0ed98069d726c71c74e507dbcb2
yamanayama <ymmtnomail@gmail.com>
2021-05-19 00:19:02 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from 'react'
+import { render } from '@testing-library/react'
+import React, { useEffect, useState } from 'react'
 
 
 const App = props => {
   const [state, setState] = useState(props)
   const { name, price } = state
+
+  useEffect(()=>{
+    console.log('compornent DidMount')
+  })
   
+  useEffect(()=>{
+    console.log('初回レンダリングのみ')
+  },[])
+  
+  
+  useEffect(()=>{
+    console.log('nameの変更があった場合のみ　collback')
+  },[name])
+
   return (
     <div>
      <p>現在の{name}は{price}</p>
