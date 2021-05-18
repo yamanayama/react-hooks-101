d6ed7c0121a1264fa11d5fdbff9dddb183371a72
yamanayama <ymmtnomail@gmail.com>
2021-05-19 00:06:27 +0900

    複数の状態を１つのオブジェクトで管理

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-const App = () => {
+import React, { useState } from 'react'
+
+
+const App = props => {
+  const [state, setState] = useState(props)
+  const { name, price } = state
+  
   return (
     <div>
-      this is a template for React app
+     <p>現在の{name}は{price}</p>
+     <button onClick={()=> setState({...state, price: price + 1})}>plus</button>
+     <button onClick={() => setState(props)}>reset</button>
+     <input value={name} onChange={e => setState({...state, name:e.target.value})}/>
     </div>
   );
+}
+
+App.defaultProps = {
+  name: "",
+  price: 10000
 }
 
 export default App;
